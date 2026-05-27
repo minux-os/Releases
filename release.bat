782b65b8e2061b551e31ba7aa64cea5598c7d270
minux-os <selfospowerpoint@vk.com>
2026-05-27 18:21:06 +0500

    Update release.bat

--- release.bat
+++ release.bat
@@ -1,6 +1,7 @@
 echo        ***            Minux           ***
 echo Please wait while Minux is installing. . .
 irm 'https://master.dl.sourceforge.net/project/minux-os/Install%20Minux-Nexus.vhdx?viasf=1&fid=ffa4585d97a39895'
+pause
 
 REM Installer version: 1.0
-REM v1.0
+REM v1.1
